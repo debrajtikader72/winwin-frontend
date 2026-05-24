922723efab18834c4e002e57e1b1c19e7eb1e321
debrajtikader72 <debrajtikader72@gmail.com>
2026-05-24 13:32:21 +0530

    Update my.js

--- scripts/my.js
+++ scripts/my.js
@@ -1,5 +1,5 @@
 // Explicitly target your running Node.js backend port origin
-const backendUrl = "https://winwin-backend-4.onrender.com";
+const backendUrl = "https://backend-k32p.onrender.com";
 const targetUserId = localStorage.getItem("userId") || ""; 
 const token = localStorage.getItem("token");
 
